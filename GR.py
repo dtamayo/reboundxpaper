7422f3dc3985c5957addf4492c0b039f71a0b9b7
dtamayo <tamayo.daniel@gmail.com>
2019-07-18 13:16:14 -0400

    updated GR scripts and figure to new rebx

--- GR.py
+++ GR.py
@@ -4,41 +4,46 @@ import numpy as np
 import sys
 import time
 
-def epic(dtfactor, integrator):
+def k2137b(dtfactor, e):
     sim = rebound.Simulation()
     sim.G = 4*np.pi**2
-    sim.add(m=0.93)
-    sim.add(m=4.5*3.e-7, P=0.571/365.25, e=0.01)
-    sim.add(m=41.*3.e-7, P=13.34/365.25, e=0.01)
+    sim.add(m=0.463)
+    sim.add(m=3.e-6, P=4.3/24/365.25, e=e)
     sim.move_to_com()
-    sim.integrator=integrator
     sim.dt = dtfactor*sim.particles[1].P
     return sim
 
-def addGR(sim, rebxintegrator, order, cfac):
+def addGR(sim, rebxintegrator, order):
+    sim.integrator = "whfast"
     rebx = reboundx.Extras(sim)
-    gr = rebx.add("gr")
-    gr.params["c"] = 63197.8*cfac # AU/yr
-    if rebxintegrator != "naive":
-        rebx.integrator=rebxintegrator
-        gr.operator_order = order 
-        gr.force_as_operator = 1
-    return rebx
+    gr = rebx.load_force("gr")
+    gr.params["c"] = 63197.8 # AU/yr
+    intf = rebx.load_operator("integrate_force")
+    intf.params['force'] = gr
+    intf.params['integrator'] = reboundx.integrators[rebxintegrator]
+    if order == 1:
+        rebx.add_operator(intf, dt_fraction=1., timing="post")
+    if order == 2:
+        rebx.add_operator(intf, dt_fraction=1./2., timing="pre")
+        rebx.add_operator(intf, dt_fraction=1./2., timing="post")
+    return rebx, gr
 
-dtfactor = float(sys.argv[1])
-integrator = sys.argv[2]
+filename = sys.argv[1]
+dtfactor = float(sys.argv[2])
 rebxintegrator = sys.argv[3]
-tmax = float(sys.argv[4])
-order = int(sys.argv[5])
-cfac = float(sys.argv[6])
-filename = 'data/GRcfac{0}{1}rebx{2}dt{3:.3e}order{4}'.format(cfac, integrator, rebxintegrator, dtfactor, order)
+order = int(sys.argv[4])
+epsfac = float(sys.argv[5])
+tmax = float(sys.argv[6])
+e = float(sys.argv[7])
+
+sim = k2137b(dtfactor=dtfactor, e=e)
+rebx, gr = addGR(sim, rebxintegrator=rebxintegrator, order=order)
+gr.params["c"] /= np.sqrt(epsfac)
+
+sim.simulationarchive_snapshot(filename+".sa")
+rebx.save(filename+'.rebx')
 
 Nout = 10000
-sim = epic(dtfactor, integrator)
-rebx = addGR(sim, rebxintegrator, order, cfac)
-rebx.save(filename+'.rebx')
-sim.simulationarchive_snapshot(filename+".sa")
-
 times = np.logspace(0, np.log10(tmax), Nout)*sim.particles[1].P
 for i, time in enumerate(times):
     sim.integrate(time, exact_finish_time=0)
--- GRscript.py
+++ GRscript.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
-call("python GR.py 0.080901699437494756 whfast euler 2e10 1 1 &", shell=True)
-call("python GR.py 0.080901699437494756 whfast rk2 2e10 1 1 &", shell=True)
-call("python GR.py 0.080901699437494756 whfast rk4 2e10 1 1 &", shell=True)
-call("python GR.py 0.080901699437494756 whfast implicit_midpoint 2e10 1 1 &", shell=True)
+call("python GR.py binaries/rk4 0.080901699437494756 rk4 1 1 1e11 0.01 &", shell=True)
+call("python GR.py binaries/rk2 0.080901699437494756 rk2 1 1 1e11 0.01 &", shell=True)
+call("python GR.py binaries/euler 0.080901699437494756 euler 1 1 1e11 0.01 &", shell=True)
+call("python GR.py binaries/implicit_midpoint 0.080901699437494756 implicit_midpoint 1 1 1e11 0.01 &", shell=True)
